edef205a6948db0f92f6a41ae4cad716e990c752
Pablo Fiallos <159185130+pablo-fiallos-granados@users.noreply.github.com>
2024-03-21 18:10:26 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Pablo Fiallos
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+
+function enterClicked() {
+  // input
+  const base = parseInt(document.getElementById("base").value)
+  const height = parseInt(document.getElementById("height").value)
+
+  // output
+  document.getElementById("user-info").innerHTML =
+    "Area = " + (base * height / 2) + " units squared"
+}
